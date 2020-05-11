7cfba729f7261ad3de166fcdf5e313a14436cf8b
Sree Harsha <Sree Harsha@LAPTOP-OI3I8J20.mshome.net>
2020-05-11 17:11:02 +0530

    added comments

--- fms-login-ms/src/main/java/com/capg/fms/user/service/IUserAccountCreationService.java
+++ fms-login-ms/src/main/java/com/capg/fms/user/service/IUserAccountCreationService.java
@@ -1,3 +1,10 @@
+/***************************************************************
+ -Author                : Keerthi Sree
+ -Created/Modified Date : 28/04/2020
+ -Description           : IUserAccountCreationService interface for 
+ 						  User Management system
+*******************************************************************/
+
 package com.capg.fms.user.service;
 
 import com.capg.fms.user.model.User;
